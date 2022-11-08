180ba29ab21e8ff8bf93102a3e74ce0398b5df17
LebArt2321 <fifa.artem.lebedev2002@mail.ru>
2022-11-08 15:05:24 +0300

    типа полурабочая связь между индексом и товарами через классы но нет

--- main/urls.py
+++ main/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('about/', views.about, name='about'),
-    path('item/', views.item, name='item'),
-    path('brand/', views.brand, name='brand'),
-    path('main/<int:pk>', views.NewDetailView.as_view(), name='test'),
-    path('main/<int:pk>', views.NewDetailView2.as_view(), name='test-2'),
+    #path('item/', views.item, name='item'),
+    #path('brand/', views.brand, name='brand'),
+    path('brand/<int:pk>', views.NewDetailView.as_view(), name='brand'),
+    path('item/<int:pk>', views.NewDetailView2.as_view(), name='tovar'),
 ]
--- main/views.py
+++ main/views.py
@@ -10,7 +10,12 @@ def index(request):
     news = Brand_s.objects.all()
     return render(request, 'main/index.html', {'news': news})
 
-   #return render(request, 'main/index.html')
+"""def index (request):
+    brand = Brand_s.objects.all()
+    context = {
+        'brand' : brand
+    }
+    return render (request,'main/index.html', context)"""
 
 def about(request):
     return render(request, 'main/about.html')
@@ -19,15 +24,34 @@ def item(request):
     return render(request, 'main/item.html')
 
 def brand(request): 
-    return render(request, 'main/brand.html')
+    return render(request, 'main/brand.html', )
+
+"""def brand (request, id):
+    brands__1 = Brand_s.objects.get(pk=id)
+    context = {
+        'brandsss': brands__1
+    }
+    session_key = request.session.session_key
+    if not session_key:
+        request.session["session_key"]=123
+        request.session.cycle_key()
+        print(request.session.session_key)
+    return render (request,'main/brand.html', context)"""
+
 
 class NewDetailView2(DetailView):
     model = Product_s
     template_name = 'main/test-product.html'
     context_object_name = 'beticle'
+    x = Brand_s.objects.all()
+    
 
+    extra_context = {'penek': Brand_s.objects.all()}
     
 class NewDetailView(DetailView):
     model = Brand_s
     template_name = 'main/test.html'
     context_object_name = 'article'
+    x = Product_s.objects.all()
+
+    extra_context = {'taburetka': Product_s.objects.all()}
