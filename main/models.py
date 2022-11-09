87c17c8e29208d93b06cd9bd15137bd76138eaa1
LebArt2321 <fifa.artem.lebedev2002@mail.ru>
2022-11-10 02:40:58 +0300

    4 commit

--- main/models.py
+++ main/models.py
@@ -1,22 +1,33 @@
 from django.db import models
+from django.urls import reverse
+
 
 class Brand_s(models.Model):
-    name = models.CharField(max_length = 128)
+    id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    name = models.CharField(max_length = 128, db_index=True)
     description = models.TextField(null=True, blank=True)
     image = models.TextField()
 
+    def get_absolute_url(self):
+        return reverse ('brand', kwargs={'brand_id':self.pk})
+        
     class Meta:
         verbose_name = 'Brand'
         verbose_name_plural = 'Brands'
-
+    
 
 class Product_s(models.Model):
     name = models.CharField(max_length = 256)
     description = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=12, decimal_places=2)
     image = models.TextField()
-    category = models.ForeignKey(Brand_s, on_delete=models.CASCADE)
+    category = models.ForeignKey(Brand_s, on_delete=models.CASCADE, null = True)
+
+    def get_absolute_url(self):
+        return reverse ('product', kwargs={'prod_id':self.pk})
 
     class Meta:
         verbose_name = 'Product'
         verbose_name_plural = 'Products'
+
+    
--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse
 from django.http import HttpResponse
 from .models import Brand_s
 from .models import Product_s
@@ -6,37 +7,62 @@ from django.views.generic import DetailView
 
 
 def index(request):
-
     news = Brand_s.objects.all()
     return render(request, 'main/index.html', {'news': news})
 
-"""def index (request):
-    brand = Brand_s.objects.all()
-    context = {
-        'brand' : brand
-    }
-    return render (request,'main/index.html', context)"""
 
 def about(request):
     return render(request, 'main/about.html')
 
-def item(request):
-    return render(request, 'main/item.html')
+def show_brand(request, brand_id):
 
-def brand(request): 
+    items = Product_s.objects.filter(category_id = brand_id)
+    brands = Brand_s.objects.all()
+
+    br = brands[brand_id-1]
+
+
+    context = {
+        'items': items,
+        'br' : br,
+    }
+
+
+    return render(request, 'main/test.html', context=context)
+
+def show_product(request, prod_id):
+
+    items = Product_s.objects.all()
+    brands = Brand_s.objects.all()
+    it = items[prod_id-1]
+
+    el = it.category
+    br = brands[1]
+
+    
+
+    
+    '''for b in brands:
+        if Brand_s.objects.filter( = b.id):
+            br = b
+            break
+    
+    br = brands[el-1]
+
+
+    for x in brands:
+        if x.name == el:
+            br = x
+            break
+
+    
+
+
+def brand(request, catid): 
     return render(request, 'main/brand.html', )
 
-"""def brand (request, id):
-    brands__1 = Brand_s.objects.get(pk=id)
-    context = {
-        'brandsss': brands__1
-    }
-    session_key = request.session.session_key
-    if not session_key:
-        request.session["session_key"]=123
-        request.session.cycle_key()
-        print(request.session.session_key)
-    return render (request,'main/brand.html', context)"""
+def item(request):
+    return render(request, 'main/item.html')
 
 
 class NewDetailView2(DetailView):
@@ -54,4 +80,10 @@ class NewDetailView(DetailView):
     context_object_name = 'article'
     x = Product_s.objects.all()
 
-    extra_context = {'taburetka': Product_s.objects.all()}
+    extra_context = {'taburetka': Product_s.objects.all()}'''
+
+    context = {
+        'it': it,
+        'br' : br,
+    }
+    return render(request, 'main/test-product.html', context=context)
